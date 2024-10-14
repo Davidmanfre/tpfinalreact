dc0a70bcb33ea7c4b74635dc8120f5319f0486d5
davidman <davidmanfredi74@gmail.com>
2024-10-13 23:15:14 -0300

    commit segunda clase

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,18 @@
+import "./navbar.css";
+
+import logo from "../../assets/LG.png";
+
 export const Navbar = () => {
   return (
-    <nav>
+    <div>
+      <img src={logo} alt="" />
+      <img src="/public/carrito-de-compras.png" alt="" />
+      <h1 className="title">Nombre</h1>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>Todas</li>
+        <li>urbanas</li>
+        <li>deportivas</li>
       </ul>
-    </nav>
+    </div>
   );
 };
